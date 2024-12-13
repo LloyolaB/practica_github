965f710d9ab7872520c9a96c5e78951190881fb7
Lloyola <luis.loyola.b@gmail.com>
2024-12-13 12:58:06 -0300

    feat: add styles and button
    -- agregamos estilos custom css
    -- anadimos un boton

--- index.php
+++ index.php
@@ -1,11 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
+
 <body>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+        }
+
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+            margin: 0;
+            padding: 10px 0;
+        }
+
+        p {
+            margin: 0;
+            padding: 10px 0;
+            background-color: #ccc;
+        }
+
+        a {
+            color: #3498db;
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+
+        button {
+            background-color: #3498db;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #2980b9;
+        }
+    </style>
     <h1>Hello World</h1>
     <p>My name is <?php echo "John Doe"; ?></p>
     <p>Today is <?php echo date('Y-m-d'); ?></p>
@@ -25,5 +77,7 @@
     <p>Today is <?php echo date('Y-m-d H:i:s', strtotime('last day of +2 months')); ?></p>
     <p>Today is <?php echo date('Y-m-d H:i:s', strtotime('first day of -2 months')); ?></p>
     <p>Today is <?php echo date('Y-m-d H:i:s', strtotime('last day of -2 months')); ?></p>
+    <button>Hola mundo</button>
 </body>
+
 </html>
